bfe827b909d47c6e029bd758caf0c929520d93a3
Jakub Hlaváček <hlavacekj22@spstrutnov.cz>
2025-05-02 12:57:03 +0200

    Propojení s databází

--- .env.example.js
+++ .env.example.js
@@ -1,3 +1,3 @@
 PORT=4000
-MONGODB_URI=mongodb+srv://<user>:<password>@cluster0.xxxx.mongodb.net/notes
-JWT_SECRET=changeMe
+MONGODB_URI=mongodb+srv://hlavacekj22:8nUbpETvJH@cluster0.0dg2vqi.mongodb.net/notes?retryWrites=true&w=majority&appName=Cluster0
+JWT_SECRET=nahodnyDlouhyRetezec
